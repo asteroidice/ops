1dd7eadb9ac11f5a488fe61b51515ad34067f5da
asteroidice <ryan.s.rabello@gmail.com>
2015-10-19 20:14:17 -0700

    Added Officer Groups

--- js/officers.js
+++ js/officers.js
@@ -1,31 +1,54 @@
-var officer = {
-  "Johnel Lagabon": {
-    position: "President",
-    imageUrl: "johnel.jpg"
+var officerTeam = {
+  "Executive Cabinet": {
+    "Johnel Lagabon": {
+      position: "President",
+      imageUrl: "johnel.jpg"
+    },
+    "Daniel Wilkinson": {
+      position: "Executive VP",
+      imageUrl: "daniel.jpg"
+    },
+    "Kyle Nicolas": {
+      position: "Social VP",
+      imageUrl: "kyle.jpg"
+    },
+    "Nathan Zimerly": {
+      position: "Marketing VP",
+      imageUrl: "nathan.jpg"
+    },
+    "Nickolas Leach": {
+      position: "Religious VP",
+      imageUrl: "nick.jpg"
+    },
+    "Kyler Morgan": {
+      position: "Secretary",
+      imageUrl: "http://lorempixel.com/400/600/cats"
+    },
+    "Jordan Dubini": {
+      position: "Financial VP",
+      imageUrl: "jordan.jpg"
+    }
   },
-  "Daniel Wilkinson": {
-    position: "Executive VP",
-    imageUrl: "daniel.jpg"
-  },
-  "Kyle Nicolas": {
-    position: "Social VP",
-    imageUrl: "kyle.jpg"
-  },
-  "Nathan Zimerly": {
-    position: "Marketing VP",
-    imageUrl: "nathan.jpg"
-  },
-  "Nickolas Leach": {
-    position: "Religious VP",
-    imageUrl: "nick.jpg"
-  },
-  "Kyler Morgan": {
-    position: "Secretary",
-    imageUrl: "//lorempixel.com/400/600/cats"
+  "Marketing Team": {
+    "Nathan Zimerly": {
+      position: "Marketing VP",
+      imageUrl: "nathan.jpg"
+    },
+    "Ryan Rabello": {
+      position: "Webmaster",
+      imageUrl: "ryan.jpg"
+    }
   }
 };
 $(document).ready(function() {
-  $.each(officer, function(key, value){
-    $('#officers').append("<div class='small-12 medium-6 large-4 columns'><img src='"+ value.imageUrl + "'/><b>" + key + "</b><br>" + value.position + "</div>");
-  })
-})
+  var dom = "";
+  $.each(officerTeam, function(k,v){
+    dom += "<div class='row'><h3>"+k+"</h3>";
+    $.each(v, function(key, value){
+      dom += "<div class='left small-12 medium-6 large-4 columns'><img src='"+ value.imageUrl + "'/><b>" + key + "</b><br>" + value.position + "</div>";
+    });
+    dom +="</div><hr>";
+  });
+  $("#officers").html(dom);
+
+});
