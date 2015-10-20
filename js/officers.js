587acc7a610c6dcbc27864252a3d3c0e8ee2489f
asteroidice <ryan.s.rabello@gmail.com>
2015-10-19 21:15:34 -0700

    Logical beatifying

--- js/officers.js
+++ js/officers.js
@@ -40,14 +40,26 @@ var officerTeam = {
     }
   }
 };
+function objLength(obj){
+  var i=0;
+  for (var x in obj){
+    if(obj.hasOwnProperty(x)){
+      i++;
+    }
+  }
+  return i;
+}
 $(document).ready(function() {
   var dom = "";
+  var num = objLength(officerTeam);
   $.each(officerTeam, function(k,v){
     dom += "<div class='row'><h3>"+k+"</h3>";
     $.each(v, function(key, value){
       dom += "<div class='left small-12 medium-6 large-4 columns'><img src='"+ value.imageUrl + "'/><b>" + key + "</b><br>" + value.position + "</div>";
     });
-    dom +="</div><hr>";
+    dom +="</div>";
+    if(num > 1) dom += "<hr>";
+    num--;
   });
   $("#officers").html(dom);
 
