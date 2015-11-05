fff6eb6c1dd9d6688bd085da0078fe4778ea69aa
asteroidice <ryan.s.rabello@gmail.com>
2015-11-04 23:02:14 -0800

    Added OPS group photo

--- js/officers.js
+++ js/officers.js
@@ -8,21 +8,12 @@ function objLength(obj){
   return i;
 }
 $(document).ready(function() {
-  /*var jqxhr = $.getJSON( "http://localhost/ops/api/data.json", function() {
-  console.log( "success" );
-})
-  .done(function() {
-    console.log( "second success" );
-  })
-  .fail(function(responce) {
-    console.log( "error", responce);
-  })*/
   var dom = "";
   var num = objLength(officerTeam);
   $.each(officerTeam, function(k,v){
     dom += "<div class='row'><h3>"+k+"</h3>";
     $.each(v, function(key, value){
-      dom += "<div class='left small-12 medium-6 large-4 columns'><a href='https://aswwu.com/#/search/" + key + "' target='_blank'><img src='"+ value.imageUrl + "'/></a><b>" + key + "</b><br>" + value.position + "</div>";
+      dom += "<div class='left small-6 medium-4 large-3 columns'><a href='https://aswwu.com/#/search/" + key + "' target='_blank'><img src='"+ value.imageUrl + "'/></a><b>" + key + "</b><br>" + value.position + "</div>";
     });
     dom +="</div>";
     if(num > 1) dom += "<hr>";
