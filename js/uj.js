bc7e9d772f68a17b97bfb88901c1807af680d295
asteroidice <ryan.s.rabello@gmail.com>
2016-01-25 20:41:23 -0800

    added new (logo and styles)

--- js/uj.js
+++ js/uj.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $.each(uj, function(key, value){
-    console.log("running UJ's");
     $('#ujs').append("<a href='" + value.link + "' target='_blank'><div>" + key + "</div></a><hr>");
   });
 });
