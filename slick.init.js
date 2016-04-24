f3e77d411faf910dd0e1a94ccfb7084da5ad632b
asteroidice <ryan.s.rabello@gmail.com>
2016-04-24 13:56:08 -0700

    added Spirit Day Slider

--- slick.init.js
+++ slick.init.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-	$("#slider").ready(function() {
-		$("#slider").slick({
-			autoplay:true,
-			autoplaySpeed: 5000,
-			pauseOnHover: false,
-			nextArrow: "",
-			prevArrow: "",
-
-		});
+	$("#slider").slick({
+		autoplay:true,
+		autoplaySpeed: 5000,
+		pauseOnHover: false,
+		nextArrow: "",
+		prevArrow: "",
+	});
+	$("#spirit").slick({
+		autoplay:true,
+		autoplaySpeed: 5000,
+		pauseOnHover: false,
+		dots: false,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  centerMode: true,
+	  variableWidth: true
 	});
 })
-	
